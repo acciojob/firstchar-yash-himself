8b9475435db24c1e76557239bb1339727d16eee5
Yash Gupta <yashgupta27690@gmail.com>
2023-08-04 20:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstChar(text) {
   // your code here
+	let char = '';
+	text = text.trim()
+if (text==='') {
+	return char
+} else {
+	return  text.charAt(0);
+}
+	
+	
 }
 
 // Do not change the code below
-
 const text = prompt("Enter text:");
 alert(firstChar(text));
